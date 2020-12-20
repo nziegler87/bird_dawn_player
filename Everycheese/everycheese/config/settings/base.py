d080079d36d0d73edeea79cc76d0a4dc60797c1c
nziegler <ziegler.n@husky.neu.edu>
2020-12-20 12:53:11 -0500

    updated admin information

--- Everycheese/everycheese/config/settings/base.py
+++ Everycheese/everycheese/config/settings/base.py
@@ -243,7 +243,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("Your name here", "your-name-here@example.com")]
+ADMINS = [("Nathanial Ziegler", "njz5001@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
