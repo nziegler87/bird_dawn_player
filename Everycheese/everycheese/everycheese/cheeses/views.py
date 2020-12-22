f326160b3c0e5957c769c44d588f806be021bcfa
nziegler <ziegler.n@husky.neu.edu>
2020-12-21 20:10:23 -0500

    cheese detail added

--- Everycheese/everycheese/everycheese/cheeses/views.py
+++ Everycheese/everycheese/everycheese/cheeses/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from .models import Cheese
 
 class CheeseListView(ListView):
+    model = Cheese
+
+class CheeseDetailView(DetailView):
     model = Cheese
