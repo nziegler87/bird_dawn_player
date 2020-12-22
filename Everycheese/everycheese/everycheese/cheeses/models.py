f130243c242cddf54534f41a507d45df2ea17f95
nziegler <ziegler.n@husky.neu.edu>
2020-12-22 17:57:41 -0500

    added foreign key for creator

--- Everycheese/everycheese/everycheese/cheeses/models.py
+++ Everycheese/everycheese/everycheese/cheeses/models.py
@@ -4,6 +4,7 @@ from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 from django_countries.fields import CountryField
 from django.urls import reverse
+from django.conf import settings
 
 class Cheese(TimeStampedModel):
 
@@ -21,6 +22,9 @@ class Cheese(TimeStampedModel):
     firmness = models.CharField("Firmness", max_length=20,
         choices=Firmness.choices, default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField("Country of Origin", blank=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+    null=True,
+    on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
